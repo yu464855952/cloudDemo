1bcedd0546ca7738afdf9ed6fd5ad2d965c84c0b
yubing <yu46485595@163.com>
2019-09-11 15:21:47 +0800

    Hystrix Dashboard (断路器：Hystrix 仪表盘)

--- servicefeignhystrix/src/main/java/com/cloud/example/servicefeignhystrix/ServicefeignhystrixApplication.java
+++ servicefeignhystrix/src/main/java/com/cloud/example/servicefeignhystrix/ServicefeignhystrixApplication.java
@@ -3,11 +3,13 @@ package com.cloud.example.servicefeignhystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ServicefeignhystrixApplication {
 
 	public static void main(String[] args) {
--- serviceribbonhystrix/src/main/java/com/cloud/example/serviceribbonhystrix/ServiceribbonhystrixApplication.java
+++ serviceribbonhystrix/src/main/java/com/cloud/example/serviceribbonhystrix/ServiceribbonhystrixApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class ServiceribbonhystrixApplication {
 
 	public static void main(String[] args) {

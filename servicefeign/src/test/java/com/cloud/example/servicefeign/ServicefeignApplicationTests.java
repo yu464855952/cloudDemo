8820aa74c61e51c3f04d2c4c922087e0ce0ae9e5
yubing <yu46485595@163.com>
2019-09-11 14:15:11 +0800

    Feign去消费服务。

--- servicefeign/src/test/java/com/cloud/example/servicefeign/ServicefeignApplicationTests.java
+++ servicefeign/src/test/java/com/cloud/example/servicefeign/ServicefeignApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SericefeignApplicationTests {
+public class ServicefeignApplicationTests {
 
 	@Test
 	public void contextLoads() {

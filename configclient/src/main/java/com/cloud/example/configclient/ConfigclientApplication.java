d185b20202cc31e3f572651a093f0522896e8812
yubing <yu46485595@163.com>
2019-09-16 10:52:23 +0800

    配置

--- configclient/src/main/java/com/cloud/example/configclient/ConfigclientApplication.java
+++ configclient/src/main/java/com/cloud/example/configclient/ConfigclientApplication.java
@@ -3,6 +3,7 @@ package com.cloud.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 @RefreshScope
 public class ConfigclientApplication {

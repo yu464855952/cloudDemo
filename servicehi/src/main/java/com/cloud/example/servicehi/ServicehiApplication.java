99704c9c144da3bfe602dd5e4ee2f0f3bee12326
yubing <yu46485595@163.com>
2019-09-17 17:58:01 +0800

    服务链路追踪

---
+++ servicehi/src/main/java/com/cloud/example/servicehi/ServicehiApplication.java
@@ -0,0 +1,52 @@
+package com.cloud.example.servicehi;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class ServicehiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicehiApplication.class, args);
+	}
+
+	private static final Logger LOG = Logger.getLogger(ServicehiApplication.class.getName());
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping("/hi")
+	public String callHome(){
+		LOG.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+	@RequestMapping("/info")
+	public String info(){
+		LOG.log(Level.INFO, "calling trace service-hi ");
+
+		return "i'm service-hi";
+
+	}
+
+
+}
